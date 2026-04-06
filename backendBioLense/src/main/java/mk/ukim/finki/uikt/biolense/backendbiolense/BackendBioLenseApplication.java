88bb64a77931ddef9f4ea01f4c635fc3d5ddb524
Mila Kirovski <kirovski.mila@gmail.com>
2026-04-06 14:56:25 +0200

    chore: initial setup for Spring Boot project

---
+++ backendBioLense/src/main/java/mk/ukim/finki/uikt/biolense/backendbiolense/BackendBioLenseApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.uikt.biolense.backendbiolense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBioLenseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendBioLenseApplication.class, args);
+    }
+
+}
---
+++ backendBioLense/src/test/java/mk/ukim/finki/uikt/biolense/backendbiolense/BackendBioLenseApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.uikt.biolense.backendbiolense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBioLenseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
